df1608d9bf534eeceade39937b42bf732c0ebe05
Nicolas-Ribeiro <nicolas.ribeiro@bandtec.com.br>
2021-11-12 19:12:03 -0300

    Conexão com a api de cadastro

--- script_acesso.js
+++ script_acesso.js
@@ -17,14 +17,40 @@ document.getElementById("signup").addEventListener("click", function () {
 function cadastro(){
     var mail = input_email.value;
     var pass_senha = input_senha.value;
-    var nome = input_nome.value;
-    var mail_entrar = input_entrar_email.value;
-    var senha_entrar = input_entrar_senha.value;
-    if(mail == "" || pass_senha == "" || nome == ""){
+    var nomeVar = input_nome.value;
+    var cepVar = input_cep.value;
+    var enderecoVar = input_endereco.value;
+    var cnpjVar = input_cnpj.value;
+    if (mail == "" || pass_senha == "" || nome == "") {
         alert("Todos os campos devem ser preenchidos");
 
+    } else {
+        fetch("/usuarios/cadastrar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nome: nomeVar,
+                email: mail,
+                senha: pass_senha,
+                cnpj: cnpjVar,
+                cep: cepVar,
+                endereco: enderecoVar
+            })
+        }).then(function (resposta) {
+
+            console.log("resposta: ", resposta);
+
+            if (resposta.ok) {
+                window.alert("Cadastro realizado com sucesso!");
+            } else {
+                throw ("Houve um erro ao tentar realizar o cadastro!");
+            }
+        }).catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
     }
-    
 }
 function entrar(event){
     // console.log(event);
